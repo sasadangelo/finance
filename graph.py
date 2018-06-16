7663f7d662f3f5c3c35a37ecbef4c45f7796393e
Salvatore D'Angelo <SDANGELO@it.ibm.com>
2018-06-16 23:28:53 +0200

    Added volume option to the graphic

--- graph.py
+++ graph.py
@@ -28,11 +28,13 @@ def get_start_date(period):
 parser = argparse.ArgumentParser()
 parser.add_argument("ticker", help="Specify the ETF ticker")
 parser.add_argument("-p", "--period", type=str, choices=['Max', '5Y', '1Y', 'YTD', '6M', '3M', '1M', '5D'], help="Specify the period of time")
+parser.add_argument("-v", "--volume", action='store_true', help="Specify the volume")
 args=parser.parse_args()
 
 etf_name=args.ticker
 etf_currency=""
 etf_period='Max' if args.period is None else args.period
+print(args.volume)
 
 print("Load quotes for ETF: " + args.ticker + " for period " + etf_period)
 
@@ -41,7 +43,7 @@ start_date = get_start_date(etf_period)
 try:
     cnx = db.connect('database/etfs.db')
     cur = cnx.cursor()
-    cur.execute('SELECT Date, Close FROM quotes WHERE Ticker="' + args.ticker + '" and Date > ?', [start_date])
+    cur.execute('SELECT Date, Close, Volume FROM quotes WHERE Ticker="' + args.ticker + '" and Date > ?', [start_date])
     all_rows = cur.fetchall()
 except Exception as e:
     print('Failed to load quotes from database:')
@@ -68,10 +70,19 @@ finally:
 days=list(zip(*all_rows))[0]
 dates = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in days]
 prices=list(zip(*all_rows))[1]
+volumes=list(zip(*all_rows))[2]
 
 style.use('ggplot')
 
-plt.plot(dates, prices)
-plt.title(etf_name)
-plt.ylabel('Prices (' + etf_currency + ')')
+if args.volume==False:
+   plt.plot(dates, prices)
+   plt.title(etf_name)
+   plt.ylabel('Prices (' + etf_currency + ')')
+else: 
+   ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+   ax1.plot(dates, prices)
+   ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
+   ax2.bar(dates, volumes)
+   ax1.set_title(etf_name)
+   ax1.set_ylabel('Prices (' + etf_currency + ')')
 plt.show()
