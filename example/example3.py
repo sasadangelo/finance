9346fe6ab8ab4e277ae995bfcc4a55ae86fd46cc
Salvatore D'Angelo <SDANGELO@it.ibm.com>
2018-06-25 07:48:06 +0200

    Added example3.py

--- example/example3.py
+++ example/example3.py
@@ -4,14 +4,14 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def normalize_data(df):
+    return df/df.ix[0,:]
 
 def plot_selected(df, columns, start_index, end_index):
     """Plot the desired columns over index values in the given range."""
     # TODO: Your code here
     # Note: DO NOT modify anything else!
-    df=df[columns,[start_index, end_index]]
-    df.plot
-    plt.show
+    plot_data(df.ix[start_index:end_index,columns],title="Selected data")
 
 def symbol_to_path(symbol, base_dir="../database/quotes"):
     """Return CSV file path given ticker symbol."""
@@ -30,7 +30,6 @@ def get_data(symbols, dates):
         dfSymbol=dfSymbol.rename(columns={'Adj Close': symbol})
         df=df.join(dfSymbol)
         df = df.dropna()
-        print(df)
 
     return df
 
@@ -44,7 +43,7 @@ def plot_data(df, title="Stock prices"):
 
 def test_run():
     # Define a date range
-    dates = pd.date_range('2017-01-22', '2017-01-26')
+    dates = pd.date_range('2017-01-22', '2017-02-26')
 
     # Choose stock symbols to read
     symbols = ['CW8.MI', 'IWRD.MI', 'SWDA.MI']
@@ -52,8 +51,10 @@ def test_run():
     # Get stock data
     df = get_data(symbols, dates)
 
+    df = normalize_data(df)
+
     # Slice and plot
-    plot_selected(df, ['CW8.MI', 'IWRD.MI'], '2017-01-22', '2017-01-24')
+    plot_selected(df, ['CW8.MI', 'IWRD.MI'], '2017-01-22', '2017-02-26')
 
 
 if __name__ == "__main__":
